8946ae40b0f355fbcd50521817fee927c9a16f63
Long Nguyen <longnguyen@gtoken.com>
2018-03-08 14:50:52 +0700

    Added decrypt flow

--- app/src/main/java/gtoken/com/nep2/MainActivity.java
+++ app/src/main/java/gtoken/com/nep2/MainActivity.java
@@ -23,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    //Data just for testing
     private static final String mRawPrivateKey = "09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE";
     private static final String mAddress = "AXoxAX2eJfJ1shNpWqUxRh3RWNUJqvQvVa";
     private static final String mPassphrase = "Satoshi";
+    private static final String mEncryptedNEP2 = "6PYN6mjwYfjPUuYT3Exajvx25UddFVLpCw4bMsmtLdnKwZ9t1Mi3CfKe8S";
 
     private static final int N = 16384;
     private static final int r = 8;
@@ -37,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        encrypt();
+        //encrypt();
+
+        decrypt(mPassphrase, mEncryptedNEP2);
     }
 
     public byte[] hexStringToByteArray(String s) {
@@ -78,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
         /*
          * Step 3: out encryptedkey
          */
-        //Split private key into 2 parts privkey[0...15] and privkey[16...31]
         byte[] privateKeyByteArray = hexStringToByteArray(mRawPrivateKey);
         Log.d(TAG, "privateKeyByteArray=" + Arrays.toString(privateKeyByteArray) + " ,length=" + privateKeyByteArray.length);
 
@@ -98,15 +101,15 @@ public class MainActivity extends AppCompatActivity {
         bb.put(flagbyte);
         bb.put(salt);
         bb.put(encryptedkey);
-        byte[] buffer = bb.array();
+        byte[] payload = bb.array();
 
         //Add checksum
-        byte[] u8Hash = SHA256HashUtil.getDoubleSHA256Hash(buffer);
-        byte[] subU8Hash = new byte[4];
-        System.arraycopy(u8Hash, 0, subU8Hash, 0, subU8Hash.length);
-        ByteBuffer bb2 = ByteBuffer.allocate(buffer.length + subU8Hash.length);
-        bb2.put(buffer);
-        bb2.put(subU8Hash);
+        byte[] payloadDoubleHash = SHA256HashUtil.getDoubleSHA256Hash(payload);
+        byte[] checkSum = new byte[4];
+        System.arraycopy(payloadDoubleHash, 0, checkSum, 0, checkSum.length);
+        ByteBuffer bb2 = ByteBuffer.allocate(payload.length + checkSum.length);
+        bb2.put(payload);
+        bb2.put(checkSum);
         byte[] encryptedPrivateKey = bb2.array();
 
         Log.d(TAG, "encryptedPrivateKey=" + Arrays.toString(encryptedPrivateKey) + " ,length=" + encryptedPrivateKey.length);
@@ -114,51 +117,37 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Base58 encryptedPrivateKey=" + base58 + " ,length=" + base58.length());
     }
 
-    private void decrypt(byte[] salt, byte[] encryptedhalf1, byte[] encryptedhalf2) {
+    private void decrypt(String passPhrase, String encryptedNEP2) {
 
-        /*
-         * Step 1: Derive a key from the passphrase using scrypt
-         */
+        //decrypt payload
+        byte[] decryptedPayload = Base58.decode(encryptedNEP2);
+
+        //decrypt salt
+        byte[] salt = Arrays.copyOfRange(decryptedPayload, 3, 7);
+        Log.d(TAG, "salt=" + Arrays.toString(salt) + " ,length=" + salt.length);
+
+        //decrypt encrypted private key
+        byte[] encryptedPrivateKey = Arrays.copyOfRange(decryptedPayload, 7, 39);
+        Log.d(TAG, "encryptedPrivateKey=" + Arrays.toString(encryptedPrivateKey) + " ,length=" + encryptedPrivateKey.length);
+
+        //Devive Scrypt key
         byte[] scryptKey = SCryptUtil.scrypt(mPassphrase, salt, N, r, p);
         Log.d(TAG, "scryptKey=" + Arrays.toString(scryptKey) + "length=" + scryptKey.length);
 
         //Split derive key into 2 parts
         byte[] derivedhalf1 = Arrays.copyOfRange(scryptKey, 0, 32);
         byte[] derivedhalf2 = Arrays.copyOfRange(scryptKey, 32, 64);
-
         Log.d(TAG, "derivedhalf1=" + Arrays.toString(derivedhalf1) + " ,length=" + derivedhalf1.length);
         Log.d(TAG, "derivedhalf2=" + Arrays.toString(derivedhalf2) + " ,length=" + derivedhalf2.length);
 
-        /*
-         * Step 2: decrypt derivedhalf1 to get XOR 1, decrypt derivedhalf2 to get XOR 2
-         *
-         */
-        byte[] xor1 = doAES256Decrypt(encryptedhalf1, derivedhalf2);
-        Log.d(TAG, "xor1=" + Arrays.toString(xor1) + " ,length=" + xor1.length);
-        byte[] xor2 = doAES256Decrypt(encryptedhalf2, derivedhalf2);
-        Log.d(TAG, "xor2=" + Arrays.toString(xor2) + " ,length=" + xor2.length);
+        //DecryptSHA256 to get XOR
+        byte[] xor = doAES256Decrypt(encryptedPrivateKey, derivedhalf2);
 
-        //Split derivedhalf1 into 2 parts
-        byte[] derivedhalf1_1 = Arrays.copyOfRange(derivedhalf1, 0, 16);
-        Log.d(TAG, "derivedhalf1_1=" + Arrays.toString(derivedhalf1_1) + " ,length=" + derivedhalf1_1.length);
-        byte[] derivedhalf1_2 = Arrays.copyOfRange(derivedhalf1, 16, 32);
-        Log.d(TAG, "derivedhalf1_2=" + Arrays.toString(derivedhalf1_2) + " ,length=" + derivedhalf1_2.length);
+        //form private key
+        byte[] privateKeyByteArray = doXor(xor, derivedhalf1);
 
-        //get privkey[0...15]
-        byte[] privkey1 = xor(xor1, derivedhalf1_1);
-        Log.d(TAG, "privkey1=" + Arrays.toString(privkey1) + " ,length=" + privkey1.length);
+        Log.d(TAG, "raw private key=" + bytesToHex(privateKeyByteArray));
 
-        //get privkey[16...31]
-        byte[] privkey2 = xor(xor2, derivedhalf1_2);
-        Log.d(TAG, "privkey2=" + Arrays.toString(privkey2) + " ,length=" + privkey2.length);
-
-        ByteBuffer bb = ByteBuffer.allocate(privkey1.length + privkey2.length);
-        bb.put(privkey1);
-        bb.put(privkey2);
-        byte[] privateKey = bb.array();
-        Log.d(TAG, "privateKey=" + Arrays.toString(privateKey) + " ,length=" + privateKey.length);
-
-        Log.d(TAG, "decrypted private key=" + bytesToHex(privateKey));
     }
 
     private byte[] doAES256Encrypt(byte[] input, byte[] key) {
@@ -187,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
         return output;
     }
 
-    private byte[] xor(byte[] data1, byte[] data2) {
+    private byte[] doXor(byte[] data1, byte[] data2) {
         // make data2 the largest...
         if (data1.length > data2.length) {
             byte[] tmp = data2;
